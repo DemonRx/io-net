7b5a9d529b07e127d0b1d5c5c7ffc4823c24b2a9
Demon <18432686+DemonRx@users.noreply.github.com>
2023-12-15 12:41:43 -0500

    Add NVIDIA guide / docker install command

--- io-net.sh
+++ io-net.sh
@@ -214,10 +214,12 @@ if [[ ! -z "$NVIDIA_PRESENT" ]]; then
         echo "nvidia-docker is enabled and working. Exiting script."
     else
         echo "nvidia-docker does not seem to be enabled. Proceeding with installations..."
+	# Refer to install guide: https://docs.nvidia.com/datacenter/cloud-native/container-toolkit/latest/install-guide.html#installing-with-apt
         curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | sudo gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.g \
 		&& curl -s -L https://nvidia.github.io/libnvidia-container/stable/deb/nvidia-container-toolkit.list | \
 		sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | \
 		sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
+  	sudo nvidia-ctk runtime configure --runtime=docker #The nvidia-ctk command modifies the /etc/docker/daemon.json file on the host. The file is updated so that Docker can use the NVIDIA Container Runtime.
         sudo apt-get update && sudo apt-get install -y nvidia-container-toolkit
         sudo systemctl restart docker 
 
